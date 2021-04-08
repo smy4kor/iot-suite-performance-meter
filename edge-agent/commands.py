2309dbc06de281da370e35685ed21468e40f7c53
Peter Vigier <Peter.Vigier@bosch-si.com>
2021-04-08 15:24:14 +0200

    moved message handling in feature, experimented with telemetry

--- edge-agent/commands.py
+++ edge-agent/commands.py
@@ -1,15 +1,48 @@
 import json
 import re
 
+
 class MeasurementData:
-    def __init__(self,id,serialNumber):
+    def __init__(self, id, serialNumber):
         self.id = id
         self.serialNumber = serialNumber
+
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
+
+class DittoResponse:
+    """A utility class that is responsible for generating response messages according to the ditto protocol."""
+
+    def __init__(self, topic, path, response_code=None):
+        self.topic = topic
+        self.path = path.replace("inbox","outbox") ## "/features/manually-created-lua-agent/outbox/messages/install"
+
+        if response_code:
+            self.status = response_code
+
+    def prepare_aknowledgement(self, ditto_correlation_id):
+        self.value = {}
+        self.headers = {
+            "response-required": False,
+            "correlation-id": ditto_correlation_id,
+            "content-type": "application/json"
+        }
+
+    def prepare_measurement_response(self, req):
+        self.headers = {
+            "response-required": False,
+            "content-type": "application/json"
+        }
+        self.value = MeasurementData(req.id, req.serialNumber)
+
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
 class DittoCommand:
-    def __init__(self, payload,topic):
+    def __init__(self, payload, topic):
         self.payload = payload
         self.mqttTopic = topic
         self.dittoTopic = payload['topic']
@@ -17,10 +50,13 @@ class DittoCommand:
         self.dittoCorrelationId = payload['headers']["correlation-id"]
         self.dittoOriginator = payload['headers']["ditto-originator"]
         self.requestHeaders = payload['headers']
-        self.featureId = self.getFeatureId()
-    
+        self.value = payload['value']
+        self.featureId = payload['headers']['ditto-message-feature-id']
+
     def getRequestId(self):
-        pattern = "req/(.*)/" ## everything between req/ and /install is the request id. Ex topic: command///req/01fp-pdid6m-12i8u431qmpi1b-1m2zqv2replies/install
+        # everything between req/ and /install is the request id.
+        # Ex topic: command///req/01fp-pdid6m-12i8u431qmpi1b-1m2zqv2replies/install
+        pattern = "req/(.*)/"
         x = re.search(pattern, self.mqttTopic)
         if x:
             return x.group(1)
@@ -28,23 +64,24 @@ class DittoCommand:
             return None
 
     def getFeatureId(self):
-        pattern = "features/(.*)/properties/" ## /features/measure-performance-feature/properties/status/request
+        pattern = "features/(.*)/properties/"  ## /features/measure-performance-feature/properties/status/request
         x = re.search(pattern, self.path)
         if x:
             return x.group(1)
         else:
             return None
+
     def getServiceInstanceId(self):
-        pattern = "service-instance.(.*).iot-" 
+        pattern = "service-instance.(.*).iot-"
         ## everything between 'service-instance.' and '.iot-'. 
         # Ex topic: iot-suite:useridhere/service-instance.abcde.iot-things@device-management
         x = re.search(pattern, self.dittoOriginator)
         if x:
             return x.group(1)
         else:
-            return None   
-    
-    def printInfo(self):
+            return None
+
+    def print_info(self):
         print("MQTT topic: " + self.mqttTopic)
         print('Ditto topic: ' + self.dittoTopic)
         print('Ditto originator: ' + self.dittoOriginator)
@@ -53,12 +90,22 @@ class DittoCommand:
         if self.featureId:
             print('Feature id: : ' + self.featureId)
         print("===")
-        
-    def getMeasurementData(self):
+
+    def get_measurement_data(self) -> MeasurementData:
         lst = []
         if 'value' not in self.payload.keys():
             return null
         id = self.payload['value']['id']
         serialNumber = self.payload['value']['serialNumber']
-        return MeasurementData(id,serialNumber)
+        return MeasurementData(id, serialNumber)
 
+    def get_response(self) -> DittoResponse:
+        """Get an acknowledge response for this command."""
+        status = 200
+        akn_path = self.path.replace("inbox", "outbox")
+        rsp = DittoResponse(self.dittoTopic, akn_path, status)
+        rsp.prepare_aknowledgement(self.dittoCorrelationId)
+        return rsp
+
+    def response_required(self) -> bool:
+        return self.payload['headers']['response-required']
--- edge-agent/start-agent.py
+++ edge-agent/start-agent.py
@@ -1,16 +1,17 @@
+import paho
 import paho.mqtt.client as mqtt
 import time
 import json
 import argparse
 
 from commands import DittoCommand
-from commands import MeasurementData
+# from commands import MeasurementData
 from device_info import DeviceInfo
-from ditto_response import DittoResponse
-from agent import Agent
+# from ditto_response import DittoResponse
+from feature import Feature
 
-agent = Agent("performanceTest", "2.0.0", 'measure-performance-feature')
-deviceInfo = DeviceInfo()
+deviceInfo = None
+meter_feature = None
 DEVICE_INFO_TOPIC = "edge/thing/response"
 MQTT_TOPIC = [(DEVICE_INFO_TOPIC, 0), ("command///req/#", 0)]
 
@@ -40,58 +41,63 @@ def on_message(client, userdata, msg):
         print(err)
 
 
-def process_event(msg):
-    '''Will process the mqtt message based on the use case. Usecase is determined by the message topic.'''
-    payloadStr = str(msg.payload.decode("utf-8", "ignore"))
-    print(payloadStr)
-    payload = json.loads(payloadStr)
+def process_event(msg: paho.mqtt.client.MQTTMessage):
+    """Will process the mqtt message based on the use case. Use case is determined by the message topic."""
+    payload_str = str(msg.payload.decode("utf-8", "ignore"))
+    print(payload_str)
+    payload = json.loads(payload_str)
+    feature_id = payload.get('headers', {}).get('ditto-message-feature-id', None)
 
     if msg.topic == DEVICE_INFO_TOPIC:
-        deviceInfo.compute(payload)
-        agent.register(client, deviceInfo)
-        print("======== Agent is ready =============")
-    elif msg.topic == "command///req//modified":
+        global deviceInfo, meter_feature
+        deviceInfo = DeviceInfo(payload)
+        meter_feature = Feature("performanceTest", "2.0.0", 'measure-performance-feature', mqtt_client, deviceInfo)
+        meter_feature.register()
+        print("======== Feature is ready =============")
+    # elif msg.topic == "command///req//modified":
+    #     handle_measurement_request(cmd)
+    elif feature_id and feature_id == meter_feature.featureId:
         cmd = DittoCommand(payload, msg.topic)
-        handle_measurement_request(cmd)
-        cmd.printInfo()
+        cmd.print_info()
+        meter_feature.handle(cmd)
 
 
-def handle_measurement_request(cmd):
-    # todo: also check
-    if cmd.featureId == agent.featureId and cmd.path.endswith('status/request'):
-        print("processing request with ditto req id: " + str(cmd.getRequestId()))
-        acknowledge(cmd)
-        send_response(cmd)
-    else:
+# def handle_measurement_request(cmd):
+#     # todo: also check
+#     if cmd.featureId == meter_feature.featureId and cmd.path.endswith('status/request'):
+#         print("processing request with ditto req id: " + str(cmd.getRequestId()))
+#         acknowledge(cmd)
+#         send_response(cmd)
+#     else:
         print("Ignoring the message received for feature: " + str(cmd.featureId))
         # else, from cache file stored with cmd.featureId and execute the scripts stored there
 
 
 # see https://wiki.bosch-si.com/pages/viewpage.action?spaceKey=MBSIOTSDK&title=Things+Protocol+Patterns
-def send_response(cmd):
-    mr = cmd.getMeasurementData()
-    print("Responding to : {},{}".format(mr.id, mr.serialNumber))
-    pth = "/features/{}/properties/status/response".format(agent.featureId)
-
-    dittoRspTopic = "{}/{}/things/twin/commands/modify".format(deviceInfo.namespace, deviceInfo.deviceId)
-    rsp = DittoResponse(dittoRspTopic, pth)
-    rsp.prepareMeasurementResponse(mr)
-    print("publishing response: " + rsp.toJson())
-    client.publish("e", rsp.toJson(), qos=1)
-
-
-def acknowledge(cmd, value=None):
-    status = 200
-    mosquittoTopic = "command///res/" + str(cmd.getRequestId()) + "/" + str(status)
-    # print("======== Sending aknowledgement for ditto requestId: %s =============" %(cmd.getRequestId()))
-    aknPath = cmd.path.replace("inbox", "outbox")  # # "/features/manually-created-lua-agent/outbox/messages/install"
-    rsp = DittoResponse(cmd.dittoTopic, aknPath, status)
-    rsp.prepareAknowledgement(cmd.dittoCorrelationId)
-    if value:
-        rsp.value = value
-
-    client.publish(mosquittoTopic, rsp.toJson())
-    print("======== Aknowledgement sent on topic " + mosquittoTopic + " =============")
+# def send_response(cmd):
+#     mr = cmd.get_measurement_data()
+#     print("Responding to : {},{}".format(mr.id, mr.serialNumber))
+#     pth = "/features/{}/properties/status/response".format(meter_feature.featureId)
+#
+#     ditto_rsp_topic = "{}/{}/things/twin/commands/modify".format(deviceInfo.namespace, deviceInfo.deviceId)
+#     rsp = DittoResponse(ditto_rsp_topic, pth)
+#     rsp.prepareMeasurementResponse(mr)
+#     print("publishing response: " + rsp.toJson())
+#     mqtt_client.publish("e", rsp.toJson(), qos=1)
+#
+#
+# def acknowledge(cmd, value=None):
+#     status = 200
+#     mosquitto_topic = "command///res/" + str(cmd.getRequestId()) + "/" + str(status)
+#     # print("======== Sending acknowledgement for ditto requestId: %s =============" %(cmd.getRequestId()))
+#     akn_path = cmd.path.replace("inbox", "outbox")  # # "/features/manually-created-lua-agent/outbox/messages/install"
+#     rsp = DittoResponse(cmd.dittoTopic, akn_path, status)
+#     rsp.prepareAknowledgement(cmd.dittoCorrelationId)
+#     if value:
+#         rsp.value = value
+#
+#     mqtt_client.publish(mosquitto_topic, rsp.toJson())
+#     print("======== Acknowledgement sent on topic " + mosquitto_topic + " =============")
 
 
 if __name__ == "__main__":
@@ -100,14 +106,12 @@ if __name__ == "__main__":
     parser.add_argument("-mp", "--mqtt_port", metavar='PORT', help="Port of the MQTT broker", type=int, default=1883)
     args = parser.parse_args()
 
-    port = args.mqtt_port
+    print("MQTT connecting to {}:{}".format(args.mqtt_host, args.mqtt_port))
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_publish = on_publish
+    mqtt_client.on_message = on_message
 
-    print("MQTT connect to {}:{}".format(args.mqtt_host, args.mqtt_port))
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_publish = on_publish
-    client.on_message = on_message
+    mqtt_client.connect(args.mqtt_host, args.mqtt_port, 60)
 
-    client.connect(args.mqtt_host, args.mqtt_port, 60)
-
-    client.loop_forever()
+    mqtt_client.loop_forever()
