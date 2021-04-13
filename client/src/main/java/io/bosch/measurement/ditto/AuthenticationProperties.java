49c66baa632428ebc0e1cf8dd300d41bcf8ff1bb
Ravindranath Sandeep <Sandeep.Ravindranath@bosch.io>
2021-04-13 13:46:09 +0200

    use lombok for auth properties
    Signed-off-by: Ravindranath Sandeep <Sandeep.Ravindranath@bosch.io>

--- client/src/main/java/io/bosch/measurement/ditto/AuthenticationProperties.java
+++ client/src/main/java/io/bosch/measurement/ditto/AuthenticationProperties.java
@@ -7,11 +7,14 @@ package io.bosch.measurement.ditto;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Data;
+
 /**
  * Things client specific configuration properties
  */
 @Configuration
 @ConfigurationProperties(prefix = "authentication")
+@Data
 public class AuthenticationProperties {
 
     private String oauthTokenEndpoint;
@@ -20,53 +23,4 @@ public class AuthenticationProperties {
     private String clientSecret;
     private String serviceInstanceId;
     private String deviceId;
-
-    public String getOauthTokenEndpoint() {
-        return oauthTokenEndpoint;
-    }
-
-    public void setOauthTokenEndpoint(final String oauthTokenEndpoint) {
-        this.oauthTokenEndpoint = oauthTokenEndpoint;
-    }
-
-    public String getWssThingsEndpointUrl() {
-        return wssThingsEndpointUrl;
-    }
-
-    public void setWssThingsEndpointUrl(final String wssThingsEndpointUrl) {
-        this.wssThingsEndpointUrl = wssThingsEndpointUrl;
-    }
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(final String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientSecret(final String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
-
-    public String getServiceInstanceId() {
-        return serviceInstanceId;
-    }
-
-    public void setServiceInstanceId(final String serviceInstanceId) {
-        this.serviceInstanceId = serviceInstanceId;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(final String deviceId) {
-        this.deviceId = deviceId;
-    }
-
 }
