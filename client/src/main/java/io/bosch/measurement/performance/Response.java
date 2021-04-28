773621a9988fa7448bb60f75c7b392339e579ac2
Ravindranath Sandeep <Sandeep.Ravindranath@bosch.io>
2021-04-28 10:50:55 +0200

    fix merge issues
    Signed-off-by: Ravindranath Sandeep <Sandeep.Ravindranath@bosch.io>

--- client/src/main/java/io/bosch/measurement/performance/Response.java
+++ client/src/main/java/io/bosch/measurement/performance/Response.java
@@ -1,9 +1,8 @@
 package io.bosch.measurement.performance;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class Response {
-    @JsonAlias("request_id")
-    private String requestId;
-    @JsonAlias("total_count")
-    private int totalCount;
-    @JsonAlias("current_no")
-    private int currentNo;
-    @JsonIgnore
-    private final long receivedTs = System.currentTimeMillis();
+    private String id;
+    private int expected;
+    private int current;
 }
